91dea5fd5804e4f9c4409e84cc35109fc1bfdce6
Danish Wani <danis.wani@haptik.ai>
2024-03-03 13:41:48 +0530

    -Logger and const modules added

---
+++ analysis/const.py
@@ -0,0 +1,5 @@
+WIKI_BASE_URL = "https://en.wikipedia.org/w/api.php"
+WIKI_TOPIC_SEARCH_URL = f"{WIKI_BASE_URL}?action=query&prop=extracts&format=json&exintro="
+COMMON_WORDS = ['the', 'is', 'in', 'at', 'which', 'on', 'a', 'this']
+
+SEARCH_HISTORY_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
